0b3c8fd345f9ef88b0317a23ffa033d865eedcbc
MD <dannycolours98@gmail.com>
2019-04-03 21:15:42 +0300

    resolved conflict

--- tickets_app/views.py
+++ tickets_app/views.py
@@ -6,11 +6,11 @@ from django.shortcuts import render
 posts = [
     {
         "titlu" : "The Trail to Oregon",
-        "regie" : "Andrei Alexandrescu",
+        "regie" : "Andrei Alexandrescu", 
         "actori" : "Mircea, Andrei, Mihai",
         "descriere" : "A comedy about the life of the people who searched for a better life by heading to Oregon",
         "locatie" : "Teatrul Maria Filotti",
-        "data" : "23 Septembrie 2019, Ora 13:30",
+        "data" : "23 Septembrie 2019, Ora 13:30",     
         "img" : "/imgs/poster1.jpg" ,
         "background": "/imgs/background1.jpg"
     },
